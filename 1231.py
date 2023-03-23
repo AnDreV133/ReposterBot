1be3d256821dedf8e001500d6a2547f2377b468a
dmitr <dmitrijkubik@gmail.com>
2023-03-23 13:14:26 +0300

    бот репостер в 0.2

---
+++ 1231.py
@@ -0,0 +1,2 @@
+# https://vc.ru/newtechaudit/384017-sozdaem-telegram-bot-s-vebhukami-na-heroku
+# https://id.heroku.com/login
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import threading
-from libs.vk_api_dir import vk_api
-from libs.vk_api_dir.vk_api.keyboard import VkKeyboard
-from libs.vk_api_dir.vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api
+from vk_api.keyboard import VkKeyboard
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-# while True
+# while True # чел с этуба сказал, что в цикл надо завернуть,
+             # чтобы бот не лёг, когда лягут сервера
 session = vk_api.VkApi(
     token="vk1.a.hqK9U9K4SrHjy2HmLvTMiT0ngKhc6qaI-fy4pIBi0zKTbR6Jv_bT5zTy2XxPC3sR6CVBy27Q22rJs7q1L7YYG6iOcYINxPf1H_JeyRMvJxRyNfLbOcvwM2ttvdeLu5lIitHEi6xxi10qHBUvXPnuDwB3N4-9QWXcQshSXGlUd2O7eoTJDHoT8nd2EH_QlbT5n1OTwzDQgI50aRbTgg87CA"
 )
@@ -12,6 +13,8 @@ longpoll = VkLongPoll(session)
 
 
 def send_msg(user_id, msg, keyboard=None):
+    # функция для отправки сообщения, клавиатура крепится вместе с сообщением,
+    # так как отправка сообщения это запрос на сервер
     post = {
         "user_id": user_id,
         "message": msg,
@@ -23,7 +26,7 @@ def send_msg(user_id, msg, keyboard=None):
 
     session.method("messages.send", post)
 
-
+# пока редуцент авось пригодится
 # def show_keyboard_welcome():
 #     kb = VkKeyboard
 #     kb.get_empty_keyboard()
@@ -31,6 +34,7 @@ def send_msg(user_id, msg, keyboard=None):
 #     kb.get_keyboard()
 
 def get_keyboard():
+    # клавиатура для вставки/удаления ссылок и настроек
     kb = VkKeyboard(one_time=True)
     kb.add_button("Add link/s")
     kb.add_line()
@@ -39,28 +43,29 @@ def get_keyboard():
 
     return kb.get_keyboard()
 
+# для настроек тоже нужна клавиатура, там будет обязательно настройка времени
 
 def create_file_for_links(user_id, links):
-    # проверка ссылки
-    # запись
-
     links.replace(",", " ").replace(";", " ").replace(".", " ").split()
-    print(links)
 
+    # так как полноценных баз данных я делать не хочу, будет учёт пользователей через файлы
+
+    # - проверка ссылок
+    # - запись ссылок в файл, где имеются ID пользователя и настройка
+    # - возвращает список из ссылок с подписью найдена ссылка или нет
 
     return []
 
 
 def add_link(user_id):
+    # потоковая функция принимает список ссылок
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.user_id == user_id:
                 #
-
-                send_msg(user_id, f"результаты:\n {text}", keyboard=get_keyboard())
-
-
-# хорошее отображение прошедших ссылок
+                text_ = create_file_for_links(0, "") # тут будет текст сообщения с
+                                                     # прошедшими и непрошедшими ссылками
+                send_msg(user_id, f"результаты:\n {text_}", keyboard=get_keyboard())
 
 def test():
     create_file_for_links(1, "https://vk.com/so_ieitus https://vk.com/gvozdibelgorod")
@@ -71,10 +76,13 @@ if __name__ == '__main__':
             text = event.text
             user_id = event.user_id
             if text == "start":
-                send_msg(user_id, "добавьте ссылочки", get_keyboard())
+                send_msg(user_id, "вот пульт управления", get_keyboard())
             elif text == "Add link/s":
                 threading.Thread(target=add_link, args=event.obj.from_id).start()
             elif text == "Del link/s":
                 pass
             elif text == "Settings":
                 pass
+
+# питон как красивая речка, только в этой речке битые стёкла, неразорвавшиеся снаряды и просто мины плавают.
+# в коде повторяются названия переменных, на что жалуется IDE, с этим надо быть аккуратнее
