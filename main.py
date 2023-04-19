423f87eeaee85ce58adcb4b29619ca23bcb98864
AnDreV133 <88576180+AnDreV133@users.noreply.github.com>
2023-04-19 22:02:04 +0300

    бот новое начало

--- main.py
+++ main.py
@@ -1,88 +1,109 @@
-import threading
+import os
 import vk_api
-from vk_api.keyboard import VkKeyboard
+import sqlite3
+from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-# while True # чел с этуба сказал, что в цикл надо завернуть,
-             # чтобы бот не лёг, когда лягут сервера
-session = vk_api.VkApi(
-    token="vk1.a.hqK9U9K4SrHjy2HmLvTMiT0ngKhc6qaI-fy4pIBi0zKTbR6Jv_bT5zTy2XxPC3sR6CVBy27Q22rJs7q1L7YYG6iOcYINxPf1H_JeyRMvJxRyNfLbOcvwM2ttvdeLu5lIitHEi6xxi10qHBUvXPnuDwB3N4-9QWXcQshSXGlUd2O7eoTJDHoT8nd2EH_QlbT5n1OTwzDQgI50aRbTgg87CA"
-)
-session_api = session.get_api()
+DEFAULT_HOURS = 5
+
+load_dotenv()
+session = vk_api.VkApi(token=os.getenv("VK_GROUP_TOKEN"))
 longpoll = VkLongPoll(session)
 
+db = sqlite3.connect("users.db")
+sql = db.cursor()
 
-def send_msg(user_id, msg, keyboard=None):
-    # функция для отправки сообщения, клавиатура крепится вместе с сообщением,
-    # так как отправка сообщения это запрос на сервер
+
+def send_msg(user_id, msg):
+    # функция для отправки сообщения пользователю
     post = {
-        "user_id": user_id,
+        "peer_id": user_id,
         "message": msg,
         "random_id": 0
     }
 
-    if keyboard is not None:
-        post["keyboard"] = keyboard
-
     session.method("messages.send", post)
 
-# пока редуцент авось пригодится
-# def show_keyboard_welcome():
-#     kb = VkKeyboard
-#     kb.get_empty_keyboard()
-#     kb.add_button("Hello")
-#     kb.get_keyboard()
 
-def get_keyboard():
-    # клавиатура для вставки/удаления ссылок и настроек
-    kb = VkKeyboard(one_time=True)
-    kb.add_button("Add link/s")
-    kb.add_line()
-    kb.add_button("Del link/s")
-    kb.add_button("Settings")
+def create_new_file(user_id, frequency_per_days,
+                    time_of_repost, mode_of_amount_reposts):
+    # ready but it can add
+    with open(f"configs_of_users/{user_id}.txt", "w") as file:
+        file.write(f"""
+                    frequency {frequency_per_days}|
+                    time {time_of_repost}|
+                    mode {mode_of_amount_reposts}|
+                    """)
 
-    return kb.get_keyboard()
-
-# для настроек тоже нужна клавиатура, там будет обязательно настройка времени
-
-def create_file_for_links(user_id, links):
-    links.replace(",", " ").replace(";", " ").replace(".", " ").split()
-
-    # так как полноценных баз данных я делать не хочу, будет учёт пользователей через файлы
-
-    # - проверка ссылок
-    # - запись ссылок в файл, где имеются ID пользователя и настройка
-    # - возвращает список из ссылок с подписью найдена ссылка или нет
-
-    return []
-
-
-def add_link(user_id):
-    # потоковая функция принимает список ссылок
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.user_id == user_id:
-                #
-                text_ = create_file_for_links(0, "") # тут будет текст сообщения с
-                                                     # прошедшими и непрошедшими ссылками
-                send_msg(user_id, f"результаты:\n {text_}", keyboard=get_keyboard())
 
 def test():
-    create_file_for_links(1, "https://vk.com/so_ieitus https://vk.com/gvozdibelgorod")
+    pass
+
+
+def add_links_sql_execute(user_id, list_of_links):
+    if not sql.execute(f"select * from main.usersT where user_id={user_id};").fetchone():
+        sql.execute(f"insert into main.usersT (user_id, range_in_hours) values ({user_id}, {DEFAULT_HOURS});")
+
+    for link in list_of_links:
+        sql.execute(f"insert into main.linksT (links_id, link) values ({user_id}, {link});")
+
+
+def del_links_sql_execute(user_id, list_of_links_for_del):
+    for link in list_of_links_for_del:
+        sql.execute(f"delete from main.linksT where links_id={user_id}, link={link};")
+
+
+def repost_by_links(user_id, hours=None):
+    pass
+
+
+def clean_history(user_id):
+    sql.execute(f"delete from main.usersT where user_id={user_id}")
+
+
+def set_hours(user_id, hours):
+    sql.execute(f"update main.usersT set range_in_hours={hours} where user_id={user_id};")
+
+
+def main():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            # получаем текст сообщения
+            temp = event.text.split()
+            command = temp[0]
+            list_arg = temp[1:] if temp[1:] else [""]
+            # получаем ID написавшего
+            user_id = event.user_id
+            # обрабатываем сообщение
+            match command:
+                case "/start", "/help":
+                    send_msg(user_id, "<описание команд>")
+                case "/see":
+                    if not list_arg[0]:
+                        repost_by_links(user_id)
+                    elif list_arg[0].isdigit():
+                        repost_by_links(user_id, int(list_arg[0]))
+                    else:
+                        send_msg(user_id, "<ошибка>")
+                case "/add":
+                    if list_arg[0]:
+                        add_links_sql_execute(user_id, list_arg)
+                    else:
+                        send_msg(user_id, "<ошибка>")
+                case "/del":
+                    if list_arg[0]:
+                        del_links_sql_execute(user_id, list_arg)
+                    else:
+                        send_msg(user_id, "<ошибка>")
+                case "/set":
+                    if list_arg[0] != "hours" and list_arg[1].isdigit():
+                        set_hours(user_id, int(list_arg[1]))
+                    else:
+                        send_msg(user_id, "<ошибка>")
+                    # ...
+
 
 if __name__ == '__main__':
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            text = event.text
-            user_id = event.user_id
-            if text == "start":
-                send_msg(user_id, "вот пульт управления", get_keyboard())
-            elif text == "Add link/s":
-                threading.Thread(target=add_link, args=event.obj.from_id).start()
-            elif text == "Del link/s":
-                pass
-            elif text == "Settings":
-                pass
-
+    main()
 # питон как красивая речка, только в этой речке битые стёкла, неразорвавшиеся снаряды и просто мины плавают.
 # в коде повторяются названия переменных, на что жалуется IDE, с этим надо быть аккуратнее
