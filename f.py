6f76d1761baca3743856499f802f894a653d3bb5
AnDreV133 <88576180+AnDreV133@users.noreply.github.com>
2023-04-22 09:00:29 +0300

    Delete f.py

--- f.py
+++
@@ -1,9 +0,0 @@
-# (¬(¬(A ⊕ C) → B) ⊕ ¬(C→A)  ⊕ B)∧D
-
-s1 = "¬A∩¬C∩D∪¬B∩¬C∩D∪A∩¬B∩¬C∪¬A∩B"
-print(s1.replace("∩", "*").replace("∪", "+"))
-# print(s1.replace("*", "∩").replace("+", "∪"))
-"¬A*¬B*D+¬A*¬C*D+¬C*B*D+C*¬B*D+A*B*D+A*C*D"
-"¬A*¬B*D+C*¬B*D+A*B*D"
-
-"¬A*¬C*D+C*¬B*D+A*C*D"
