7e100b090a3d5c2586c96180a144223499e7902c
AnDreV133 <88576180+AnDreV133@users.noreply.github.com>
2023-06-02 22:45:16 +0300

    работа с багами

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import vk_api
 import sqlite3
 from dotenv import load_dotenv
+from vk_api import utils
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-DEFAULT_NUM_OF_POSTS = 2
-DEFAULT_SIZE = 200
+DFT_NUM_OF_POSTS = 2
+DFT_SIZE = 200
 
 load_dotenv()
 
@@ -17,41 +18,51 @@ app = vk_api.VkApi(token=os.getenv("VK_APP_TOKEN")).get_api()
 db = sqlite3.connect("mainDB.db")
 sql = db.cursor()
 
+sql.execute("begin;")
+# user_id | num_of_posts | size
+# table of users with personal settings
 sql.execute(
     """
-    create table if not exists usersT (
+    create table if not exists usersT ( 
       user_id unsigned big int primary key not null,
       num_of_posts unsigned big int,
       size int
     );
     """)
+
+# sql.execute("drop table linksT")
+
+# user_id | link
+# table stores links for user
 sql.execute(
     """
     create table if not exists linksT (
-      links_id unsigned big int not null,
-      link text,
-      foreign key (links_id) references usersT(user_id) on delete cascade 
+      user_id unsigned big int not null,
+      link varchar(50),
+      
+      foreign key (user_id) references usersT(user_id) on delete cascade 
     );
     """
 )
+
+# fields of links becomes unique for each user
+sql.execute("DROP INDEX IF EXISTS idx_links;")
+sql.execute("CREATE UNIQUE INDEX idx_links ON linksT (user_id, link);")
+
+sql.execute("commit;")
 
 print(sql.execute("select * from usersT").fetchall())
 print(sql.execute("select * from linksT").fetchall())
 
 
-def create_new_file(user_id, frequency_per_days,
-                    time_of_repost, mode_of_amount_reposts):
-    # ready but it can add
-    with open(f"configs_of_users/{user_id}.txt", "w") as file:
-        file.write(f"""
-                    frequency {frequency_per_days}|
-                    time {time_of_repost}|
-                    mode {mode_of_amount_reposts}|
-                    """)
-
-
 def test():
     pass
+
+
+def handle_error(user_id, err_msg, execute, log_msg):
+    send_msg(user_id, err_msg)
+    sql.execute(execute)
+    print(log_msg)
 
 
 def send_msg(user_id, msg):
@@ -70,8 +81,9 @@ def send_repost(user_id, link, settings_of_user):
     domain = link[link.rfind('/') + 1:]
     num_of_posts = settings_of_user[1]
     size = settings_of_user[2]
+
     items = app.wall.get(domain=domain, count=num_of_posts)['items']
-    msg_text = app.groups.getById(group_id=domain)[0]['name'] + '\n' + link + "\n\n----\n"
+    msg_text = app.groups.getById(group_id=domain)[0]['name'] + '\n' + link + "\n\n----\n\n"
     for item in items:
         link_to_post = f"https://vk.com/wall{item['owner_id']}_{item['id']}" + '\n'
         text_of_post = item['text'][:size].replace('\n', ' ') + "...\n\n"
@@ -83,22 +95,31 @@ def send_repost(user_id, link, settings_of_user):
 
 def add_links_sql_execute(user_id, list_of_links):
     if not sql.execute(f"select * from usersT where user_id={user_id};").fetchone():
-        sql.execute(f"insert into usersT (user_id, num_of_posts, size) values ({user_id}, {DEFAULT_NUM_OF_POSTS}, {DEFAULT_SIZE});")
+        sql.execute("begin;")
+        sql.execute(
+            f"insert into usersT (user_id, num_of_posts, size) values ({user_id}, {DFT_NUM_OF_POSTS}, {DFT_SIZE});"
+        )
+        sql.execute("commit;")
 
+    sql.execute("begin;")
     for link in list_of_links:
-        sql.execute(f"insert into linksT (links_id, link) values ({user_id}, '{link}');")
+        if app.groups.getById:
+        sql.execute(f"insert or ignore into linksT (user_id, link) values ({user_id}, '{link}');")
+    sql.execute("commit;")
 
     print(sql.execute("select * from usersT").fetchall())
     print(sql.execute("select * from linksT").fetchall())
 
 
 def del_links_sql_execute(user_id, list_of_links_for_del):
+    sql.execute("begin;")
     for link in list_of_links_for_del:
-        sql.execute(f"delete from linksT where links_id={user_id} and link='{link}';")
+        sql.execute(f"delete from linksT where user_id={user_id} and link='{link}';")
+    sql.execute("commit;")
 
 
 def repost_by_links(user_id, size=None, num_of_posts=None):
-    # определяем время за которое вышли посты
+    # get settings
     settings_of_user = sql.execute(f"select * from usersT where user_id={user_id};").fetchone()
 
     if size:
@@ -107,7 +128,7 @@ def repost_by_links(user_id, size=None, num_of_posts=None):
         settings_of_user[2] = num_of_posts
 
     # получаем список ссылок сообществ
-    links = list(map(lambda x: x[0], sql.execute(f"select link from linksT where links_id={user_id};").fetchall()))
+    links = list(map(lambda x: x[0], sql.execute(f"select link from linksT where user_id={user_id};").fetchall()))
 
     print(links)
     for link in links:
@@ -115,11 +136,15 @@ def repost_by_links(user_id, size=None, num_of_posts=None):
 
 
 def clean_history(user_id):
+    sql.execute("begin;")
     sql.execute(f"delete from usersT where user_id={user_id}")
+    sql.execute("commit;")
 
 
 def set_num_of_posts(user_id, num_of_posts):
+    sql.execute("begin;")
     sql.execute(f"update usersT set num_of_posts={num_of_posts} where user_id={user_id};")
+    sql.execute("commit;")
 
 
 def main():
@@ -165,7 +190,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main() # сделать сохранение в базу данных
+    main()
 
-# питон как красивая речка, только в этой речке битые стёкла, неразорвавшиеся снаряды и просто мины плавают.
-# в коде повторяются названия переменных, на что жалуется IDE, с этим надо быть аккуратнее
+# питон? как красивая речка, дно которой усеяно битыми стёклами, неразорвавшимися снарядами и просто плавающими минами.
